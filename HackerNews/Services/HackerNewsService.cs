f7caa39623958d4ff1bc113c00b83dfb0ac93dbb
Robert Fleischmann <bboombotz@gmail.com>
2018-04-14 10:37:54 -0400

    Implemented Caching
    Caching HTTP response data from Hacker News API for one minute

--- HackerNews/Services/HackerNewsService.cs
+++ HackerNews/Services/HackerNewsService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Runtime.Caching;
+using System.IO;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -12,27 +12,42 @@ namespace HackerNews.Services
 {
     public class HackerNewsService
     {
+        // Define our cache timeout value
+        const double CACHETIMEOUT = 60.0;
+
         /// <summary>
         /// Gets the list of story IDs from the HackerNews API
         /// </summary>
         /// <returns></returns>
         public static async Task<List<int>> GetBestStoriesIDs()
         {
-            var stories = new List<int>();
+            // Setup variables, cache, API URL
+            ObjectCache cache = MemoryCache.Default;
             var respString = "";
-            // Get the API URL for retrieving best story IDs
+            var stories = cache["storyIDs"] as List<int>;
             var apiURL = ConfigurationManager.AppSettings["HNBestStoriesURL"];
 
-            using (HttpClient httpClient = new HttpClient())
+            // Check the cache if we already have our Story IDs
+            if ( stories == null )
             {
-                // Await the response from the cient API
-                HttpResponseMessage response = await httpClient.GetAsync(apiURL);
+                // Setup the expiration for the cache
+                CacheItemPolicy policy = new CacheItemPolicy();
+                policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(CACHETIMEOUT);
 
-                if(response.IsSuccessStatusCode)
+                // Now get the IDs from the API
+                using (HttpClient httpClient = new HttpClient())
                 {
-                    respString = await response.Content.ReadAsStringAsync();
-                    stories = JsonConvert.DeserializeObject<List<int>>(respString);
+                    // Await the response from the cient API
+                    HttpResponseMessage response = await httpClient.GetAsync(apiURL);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        respString = await response.Content.ReadAsStringAsync();
+                        stories = JsonConvert.DeserializeObject<List<int>>(respString);
+                    }
                 }
+                // And now cache the data
+                cache.Set("storyIDs", stories, policy);
             }
             return stories;
         }
@@ -42,28 +57,42 @@ namespace HackerNews.Services
             // No IDs, no results
             if(storyIDs.Count == 0) { return new List<BestStoriesModel>(); }
 
-            // Get the URL to get story details
+            // Setup our cache, API URLs and variables
+            ObjectCache cache = MemoryCache.Default;
+            var bestStores = cache["bestStories"] as List<BestStoriesModel>;
             var apiURL = ConfigurationManager.AppSettings["HNStoryDetailsURL"];
 
-            // Loop through our story IDs and generate a list of best stories
-            var bestStores = new List<BestStoriesModel>();
-            using (HttpClient httpClient = new HttpClient())
+            // Check the cache if we already have the stories stored
+            if (bestStores == null)
             {
-                foreach (var storyID in storyIDs)
-                {
-                    // Await the response from the cient API
-                    HttpResponseMessage response = await httpClient.GetAsync(apiURL + storyID + ".json");
+                // Setup the expiration for the cache
+                CacheItemPolicy policy = new CacheItemPolicy();
+                policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(CACHETIMEOUT);
 
-                    if (response.IsSuccessStatusCode)
+                // Setup our best stories list object
+                bestStores = new List<BestStoriesModel>();
+
+                // Loop through our story IDs and generate a list of best stories          
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    foreach (var storyID in storyIDs)
                     {
-                        var respData = await response.Content.ReadAsStringAsync();
-                        var story = JsonConvert.DeserializeObject<HNStoryModel>(respData);
-                        var hnstory = new BestStoriesModel();
-                        hnstory.author = story.by;
-                        hnstory.title = story.title;
-                        bestStores.Add(hnstory);
+                        // Await the response from the cient API
+                        HttpResponseMessage response = await httpClient.GetAsync(apiURL + storyID + ".json");
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var respData = await response.Content.ReadAsStringAsync();
+                            var story = JsonConvert.DeserializeObject<HNStoryModel>(respData);
+                            var hnstory = new BestStoriesModel();
+                            hnstory.author = story.by;
+                            hnstory.title = story.title;
+                            bestStores.Add(hnstory);
+                        }
                     }
                 }
+                // And now cache the data
+                cache.Set("bestStories", bestStores, policy);
             }
             return bestStores;
         }
