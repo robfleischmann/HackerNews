f8799c9e8f25ebe2f274ee0ff4c11f4cb68538ab
Robert Fleischmann <robertfleischmann@usicllc.com>
2018-04-14 01:28:35 -0400

    Initial Project Setup And API Load Working
    Initial MVC project created and working data load (best stories) from HackerNews API

---
+++ HackerNews/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using HackerNews.Services;
+
+namespace HackerNews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var bestStoriesIDs = await HackerNewsService.GetBestStoriesIDs();
+            var bestStories = await HackerNewsService.GetBestStories(bestStoriesIDs);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ HackerNews/Services/HackerNewsService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Configuration;
+using HackerNews.Models;
+
+namespace HackerNews.Services
+{
+    public class HackerNewsService
+    {
+        /// <summary>
+        /// Gets the list of story IDs from the HackerNews API
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<List<int>> GetBestStoriesIDs()
+        {
+            var stories = new List<int>();
+            var respString = "";
+            // Get the API URL for retrieving best story IDs
+            var apiURL = ConfigurationManager.AppSettings["HNBestStoriesURL"];
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // Await the response from the cient API
+                HttpResponseMessage response = await httpClient.GetAsync(apiURL);
+
+                if(response.IsSuccessStatusCode)
+                {
+                    respString = await response.Content.ReadAsStringAsync();
+                    stories = JsonConvert.DeserializeObject<List<int>>(respString);
+                }
+            }
+            return stories;
+        }
+
+        public static async Task<List<BestStoriesModel>> GetBestStories(List<int> storyIDs)
+        {
+            // No IDs, no results
+            if(storyIDs.Count == 0) { return new List<BestStoriesModel>(); }
+
+            // Get the URL to get story details
+            var apiURL = ConfigurationManager.AppSettings["HNStoryDetailsURL"];
+
+            // Loop through our story IDs and generate a list of best stories
+            var bestStores = new List<BestStoriesModel>();
+            using (HttpClient httpClient = new HttpClient())
+            {
+                foreach (var storyID in storyIDs)
+                {
+                    // Await the response from the cient API
+                    HttpResponseMessage response = await httpClient.GetAsync(apiURL + storyID + ".json");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var respData = await response.Content.ReadAsStringAsync();
+                        var story = JsonConvert.DeserializeObject<HNStoryModel>(respData);
+                        var hnstory = new BestStoriesModel();
+                        hnstory.author = story.by;
+                        hnstory.title = story.title;
+                        bestStores.Add(hnstory);
+                    }
+                }
+            }
+            return bestStores;
+        }
+    }
+}
